d5b306a500f5e4c95ce27bd186bee500999c9ec3
niko <djan.mail@gmail.com>
2014-12-12 01:03:43 +0200

    russian translation - begin

---
+++ X2048_ru.ts
@@ -0,0 +1,279 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Game</name>
+    <message>
+        <source>Please go to Settings page to unlock this feature.</source>
+        <translation>Зайдите в настройки для разблокировки этой функции.</translation>
+    </message>
+</context>
+<context>
+    <name>main</name>
+    <message>
+        <source>Review</source>
+        <translation>Обзор</translation>
+    </message>
+    <message>
+        <source>New Game</source>
+        <translation>Новая игра</translation>
+    </message>
+    <message>
+        <source>You Win</source>
+        <translation>Победа</translation>
+    </message>
+    <message>
+        <source>Game Over</source>
+        <translation>Проигрыш</translation>
+    </message>
+    <message>
+        <source>Continue</source>
+        <translation>Продолжить</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Перезапуск</translation>
+    </message>
+    <message>
+        <source>Share Screenshot</source>
+        <translation>Поделиться скриншотом</translation>
+    </message>
+    <message>
+        <source>Screenshot</source>
+        <translation>Скриншон</translation>
+    </message>
+    <message>
+        <source>Tutorial</source>
+        <translation>Пособие</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation>Отмена</translation>
+    </message>
+</context>
+<context>
+    <name>settings</name>
+    <message>
+        <source>Settings</source>
+        <translation>Настройки</translation>
+    </message>
+    <message>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+    <message>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message>
+        <source>You may specify a different language here, unsupported language would be displayed as English.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Simplified Chinese</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Traditional Chinese</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>English</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Indonesian</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Spanish</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Italiano</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Deutsch</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>Animation Speed</source>
+        <translation>Скорость анимации</translation>
+    </message>
+    <message>
+        <source>Choose the animation speed.</source>
+        <translation>Выбери скорость анимации.</translation>
+    </message>
+    <message>
+        <source>Slow</source>
+        <translation>Медленно</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Нормально</translation>
+    </message>
+    <message>
+        <source>Fast</source>
+        <translation>Быстро</translation>
+    </message>
+    <message>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message>
+        <source>You can choose the theme here, no restart required.</source>
+        <translation>Ты можешь выбрат тему здесь, перезапуск не нужен.</translation>
+    </message>
+    <message>
+        <source>Dark</source>
+        <translation>Темная</translation>
+    </message>
+    <message>
+        <source>Bright</source>
+        <translation>Светлая</translation>
+    </message>
+    <message>
+        <source>Vivid</source>
+        <translation>Яркая</translation>
+    </message>
+    <message>
+        <source>Blue</source>
+        <translation>Голубая</translation>
+    </message>
+    <message>
+        <source>Flat Dots</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unlock</source>
+        <translation type="unfinished">Разблокировать</translation>
+    </message>
+    <message>
+        <source>Undo Feature Status:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unlocked</source>
+        <translation type="unfinished">Разблокированно</translation>
+    </message>
+    <message>
+        <source>Locked</source>
+        <translation type="unfinished">Заблокированно</translation>
+    </message>
+    <message>
+        <source>Undo feature lets you undo your last 50 movements, till the very beginning of the game,or the last saved state.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unlock UNDO Feature</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>If you already purchased the UNDO feature via BlackBerry World, press the button below :</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Refresh Payment Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thank you for your support.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>No Record.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Waiting for Payment Service...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Your KEY is: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source> ,Please enter your unlock code below:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid Unlock Code</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>BBM connection failed, Please Allow BBM Connecting, restart app and try again. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Undo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Team</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Français</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Bug Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Submit Translation</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>team</name>
+    <message>
+        <source>Back</source>
+        <translation>Назад</translation>
+    </message>
+</context>
+<context>
+    <name>tutorial</name>
+    <message>
+        <source>Swipe your &lt;strong&gt;finger&lt;/strong&gt; to move the tiles. When two tiles with the same number touch, they &lt;strong&gt;merge into one!&lt;/strong&gt;</source>
+        <translation>Двигай &lt;strong&gt;пальцем&lt;/strong&gt; чтобы двигать плитки. Когда две плитки с одинаковым номером соприкоснуться - они будут&lt;strong&gt; объеденены в одну!&lt;/strong&gt;</translation>
+    </message>
+    <message>
+        <source>HOW TO PLAY</source>
+        <translation>КАК ИГРАТЬ</translation>
+    </message>
+    <message>
+        <source>PLAY</source>
+        <translation>ИГРАТЬ</translation>
+    </message>
+    <message>
+        <source>When you created a 2048 tile, you win.</source>
+        <translation>Для выигрыша собери 2048 плитку.</translation>
+    </message>
+    <message>
+        <source>ACCESS SETTINGS</source>
+        <translation>Доступ</translation>
+    </message>
+    <message>
+        <source>Swipe down from the top of screen, you&apos;ll see the actions available.</source>
+        <translation>Проведи пальцем от верха экрана для простмотра доступных действий.</translation>
+    </message>
+    <message>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+    <message>
+        <source>Tutorial</source>
+        <translation>Пособие</translation>
+    </message>
+</context>
+</TS>
